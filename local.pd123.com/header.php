3afbc46d8ed71fe72480e20ecb0e770768789a2f
Nazar <fedunnazar20122001@gmail.com>
2023-06-06 10:20:29 +0300

    add links to the login and register pages

--- local.pd123.com/header.php
+++ local.pd123.com/header.php
@@ -1,21 +1,17 @@
 <header data-bs-theme="dark">
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Carousel</a>
+            <a class="navbar-brand" href="#">PHP-сайт</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="/index.php">Головна</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li>
+                </ul>
+                <ul class="navbar-nav me-3">
                     <li class="nav-item">
                         <a class="nav-link" href="/auth/register.php"> Реєстрація</a>
                     </li>
@@ -23,10 +19,6 @@
                         <a class="nav-link" href="/auth/login.php">Вхід</a>
                     </li>
                 </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
             </div>
         </div>
     </nav>
