7c60558892093dcdeac0415633aad08fd71b862f
Nazar <fedunnazar20122001@gmail.com>
2023-06-09 12:22:33 +0300

    bag with uploading image. Smth with destination

--- local.pd123.com/auth/register.php
+++ local.pd123.com/auth/register.php
@@ -5,6 +5,7 @@ $email = "";
 $phone = "";
 $password = "";
 if($_SERVER["REQUEST_METHOD"]==="POST") {
+    echo "<br/><br/><br/> POST METHOD";
     if(isset($_POST["name"])) // перевіряє присутність змінної в масиві
         $name = $_POST["name"]; // супер глобальний масив, який зберігає значення полів форми
     if (isset($_POST["surname"]))
@@ -13,25 +14,54 @@ if($_SERVER["REQUEST_METHOD"]==="POST") {
         $email = $_POST["email"];
     if(isset($_POST["password"]))
         $password = $_POST["password"];
-    if (isset($_POST["phone"])) {
+    if (isset($_POST["phone"]))
         $phone = $_POST["phone"];
-    }
-    if(!empty($name) && !empty($surname) && !empty($email) && !empty($password)) {
-        try {
-            // Підключення до Бази Даних
-            $dbh = new PDO('mysql:host=localhost;dbname=pd123', "root", "");
-            // Створює запит до БД
-            $sql = "INSERT INTO users (name, surname, email, password, phone) VALUES(?, ?, ?, ?, ?);";
-            $stmt = $dbh->prepare($sql); // створити параметризований запит
-            $stmt->execute([$name, $surname, $email, $password, $phone]);
-            $dbh = null;
-            header('Location: /'); // перехід на головну сторінку
-            exit;
-        } catch (PDOException $e) {
-            print "Error Register!: " . $e->getMessage() . "<br/>";
+    if (isset($_FILES["file"])) {
+        $file = $_FILES["file"];
+        $fileName = $file['name'];
+        $fileTmpName = $file['tmp_name']; // file location
+        $fileSize = $file['size'];
+        $fileError = $file['error'];
+        $fileType = $file['type'];
+        echo "<br/><br/><br/>$fileName<br/>$fileTmpName<br/>$fileSize<br/>$fileError<br/>$fileType";
+
+        $fileExt = explode('.', $fileName);
+        $fileActualExt = strtolower($fileExt[1]); // end($fileExt)
+        echo "<br/> actExt -> $fileActualExt";
+
+        $allowedExt = array('jpg', 'jpeg', 'png', 'gif');
+
+        if (!in_array($fileActualExt, $allowedExt)) {
+            // Wrong file extension
+            // make image red border
             die();
         }
+        if ($fileError !== 0) {
+            // There was an error uploading your file;
+            die();
+        }
+
+        $fileNameNew = uniqid().'.'.$fileActualExt; // get type format in seconds from actual time + . + file extension
+        $fileDestination = '../assets/useImages/'.$fileNameNew;
+        move_uploaded_file($fileTmpName, $fileDestination);
     }
+
+//    if(!empty($name) && !empty($surname) && !empty($email) && !empty($password) && !empty($file)) {
+//        try {
+//            // Підключення до Бази Даних
+//            $dbh = new PDO('mysql:host=localhost;dbname=pd123', "root", "");
+//            // Створює запит до БД
+//            $sql = "INSERT INTO users (name, surname, email, password, phone) VALUES(?, ?, ?, ?, ?);";
+//            $stmt = $dbh->prepare($sql); // створити параметризований запит
+//            $stmt->execute([$name, $surname, $email, $password, $phone]);
+//            $dbh = null;
+//            header('Location: /'); // перехід на головну сторінку
+//            exit;
+//        } catch (PDOException $e) {
+//            print "Error Register!: " . $e->getMessage() . "<br/>";
+//            die();
+//        }
+//    }
 }
 ?>
 
@@ -54,6 +84,8 @@ include $_SERVER["DOCUMENT_ROOT"]."/header.php";
     <div class="container">
         <h1 class="text-center">Реєстрація</h1>
         <form method="POST" enctype="multipart/form-data" class="mt-5 col-md-6 offset-md-3 needs-validation" novalidate>
+
+            <!-- Ім'я та Фамілія  -->
             <div class="mb-3 row">
                 <div class="col-md-6">
                     <label for="name" class="form-label" >Ім'я</label>
@@ -87,6 +119,7 @@ include $_SERVER["DOCUMENT_ROOT"]."/header.php";
                 </div>
             </div>
 
+            <!-- Пошта -->
             <div class="mb-3">
                 <label for="email" class="form-label">Пошта</label>
                 <input
@@ -103,6 +136,7 @@ include $_SERVER["DOCUMENT_ROOT"]."/header.php";
                 </div>
             </div>
 
+            <!-- Телефон -->
             <div class="mb-3">
                 <label for="phone" class="form-label">Телефон</label>
                 <input
@@ -114,6 +148,7 @@ include $_SERVER["DOCUMENT_ROOT"]."/header.php";
                 >
             </div>
 
+            <!-- Пароль -->
             <div class="mb-3">
                 <label for="password" class="form-label">Пароль</label>
                 <input
@@ -128,6 +163,7 @@ include $_SERVER["DOCUMENT_ROOT"]."/header.php";
                 </div>
             </div>
 
+            <!-- Фотографія -->
             <div class="mb-3">
                 <label class="form-label" for="file">
                     Оберіть фотографію
