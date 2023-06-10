5216ffdacd4fb38489b73ad0678806695461806f
Nazar <fedunnazar20122001@gmail.com>
2023-06-10 12:17:43 +0300

    update includes

--- local.pd123.com/login.php
+++ local.pd123.com/login.php
@@ -25,21 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 }
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="../css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="../css/site.css">
-</head>
-<body>
-<?php
-include $_SERVER["DOCUMENT_ROOT"]."/header.php";
-?>
+<?php include $_SERVER["DOCUMENT_ROOT"]."/head.php"?>
 <main class="mt-3">
     <div class="container">
         <h1 class="text-center">Вхід</h1>
@@ -66,7 +52,5 @@ include $_SERVER["DOCUMENT_ROOT"]."/header.php";
         </form>
     </div>
 </main>
-<script src="../js/bootstrap-validation.js"></script>
-<script src="../js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+<script src="js/bootstrap-validation.js"></script>
+<?php include $_SERVER["DOCUMENT_ROOT"]."/footer.php" ?>
