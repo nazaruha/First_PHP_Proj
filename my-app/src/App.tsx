0e6ac40d3a0351516084ffa1cb23d177d251d259
Nazar <fedunnazar20122001@gmail.com>
2023-06-30 00:38:56 +0300

    replace routes into

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,37 @@
-import { Link } from 'react-router-dom';
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
 import './App.css';
+import DefaultLayout from "./containers/default/DefaultLayout";
+import HomePage from "./components/admin/home/HomePage";
+import AdminHomePage from "./components/admin/home/AdminHomePage";
+import CategoryListPage from "./components/admin/categories/list/CategoryListPage";
+import CategoryCreatePage from "./components/admin/categories/create/CategoryCreatePage";
+import CategoryEditPage from "./components/admin/categories/edit/CategoryEditPage";
+import React from "react";
+import AdminLayout from "./components/admin/container/AdminLayout";
 
 const App = () => {
 
   return (
     <>
-      <h1 className='text-center'>App.tsx</h1>
-      <Link to="/admin/category">Категорії</Link>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<DefaultLayout/>}>
+                    <Route index element={<HomePage/>}/>
+                </Route>
+
+                <Route path="/admin" element={<AdminLayout />}>
+                    <Route index element={<AdminHomePage/>}/>
+                    <Route path="category">
+                        <Route index element={<CategoryListPage/>}/>
+                        <Route path="create" element={<CategoryCreatePage/>}/>
+                        {/*<Route path="edit/:id" element={} />*/}
+                        <Route path="edit">
+                            <Route path={":id"} element={<CategoryEditPage/>}/>
+                        </Route>
+                    </Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </>
   );
 }
