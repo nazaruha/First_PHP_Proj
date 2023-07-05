141401c6e55f008d72d107e4c045ae01311c66a0
Nazar <fedunnazar20122001@gmail.com>
2023-07-05 17:35:07 +0300

    make auth routes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,6 @@
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
 import './App.css';
-import DefaultLayout from "./containers/default/DefaultLayout";
+import DefaultLayout from "./components/containers/DefaultLayout";
 import HomePage from "./components/admin/home/HomePage";
 import AdminHomePage from "./components/admin/home/AdminHomePage";
 import CategoryListPage from "./components/admin/categories/list/CategoryListPage";
@@ -8,18 +8,21 @@ import CategoryCreatePage from "./components/admin/categories/create/CategoryCre
 import CategoryEditPage from "./components/admin/categories/edit/CategoryEditPage";
 import React from "react";
 import AdminLayout from "./components/admin/container/AdminLayout";
+import LoginPage from "./components/auth/login/LoginPage";
+import RegisterPage from "./components/auth/register/RegisterPage";
 
 const App = () => {
 
-  return (
-    <>
-        <BrowserRouter>
+    return (
+        <>
             <Routes>
                 <Route path="/" element={<DefaultLayout/>}>
                     <Route index element={<HomePage/>}/>
+                    <Route path="login" element={<LoginPage/>}/>
+                    <Route path="register" element={<RegisterPage/>}/>
                 </Route>
 
-                <Route path="/admin" element={<AdminLayout />}>
+                <Route path="/admin" element={<AdminLayout/>}>
                     <Route index element={<AdminHomePage/>}/>
                     <Route path="category">
                         <Route index element={<CategoryListPage/>}/>
@@ -31,9 +34,8 @@ const App = () => {
                     </Route>
                 </Route>
             </Routes>
-        </BrowserRouter>
-    </>
-  );
+        </>
+    );
 }
 
 export default App;
