65d7487a68bf6bcf4b937737c716cf9c5a4ab066
nazaruha <39126639+nazaruha@users.noreply.github.com>
2023-12-04 10:07:19 +0100

    put the comma

--- local.pd123.com/connection_database.php
+++ local.pd123.com/connection_database.php
@@ -2,7 +2,7 @@
 require_once "config.php"; // connect config file
 
 try {
-    // Підключення до Бази Даних з параметрами, які створили в config.php (DB_DRIVERm DB_HOST etc...)
+    // Підключення до Бази Даних з параметрами, які створили в config.php (DB_DRIVER, DB_HOST etc...)
     //$dbh = new PDO('mysql:host=localhost;dbname=pd123', "root", "");
     $dbh = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD,
         array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES " . DB_CHARSET)); // подключаєм кодіровку данних
